60f2bd28f79cf66a73b54710e64fafd1e29e4b4f
Wilhelm Hansson <wille.live@gmail.com>
2020-03-07 13:03:54 +0100

    added DB and migration

--- config.py
+++ config.py
@@ -1,3 +1,6 @@
 import os
+basedir = os.path.abspath(os.path.dirname(__file__))
 class Config(object):
     SECRET_KEY = os.environ.get('SECRET_KEY') or 'shereetai0poo!s1yee#G4ei2:bu'
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or 'sqlite:///' + os.path.join(basedir,'app.db')
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
---
+++ server/models.py
@@ -0,0 +1,12 @@
+from server import db
+
+
+class Product(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    product_nr = db.Column(db.String(50), index=True, unique=True)
+    name = db.Column(db.String(100), index=True)
+    price = db.Column(db.Integer)
+
+    def __repr__(self):
+        return '<Product {} | {} | {}>'.format(self.id,self.product_nr,self.name)
+
