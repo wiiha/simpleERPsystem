fd0a3317f698e584d8473b9cf2ae2fb8384e3ca5
Wilhelm Hansson <wille.live@gmail.com>
2020-03-07 12:41:03 +0100

    config file added and basic template

---
+++ config.py
@@ -0,0 +1,3 @@
+import os
+class Config(object):
+    SECRET_KEY = os.environ.get('SECRET_KEY') or 'shereetai0poo!s1yee#G4ei2:bu'
--- server/routes.py
+++ server/routes.py
@@ -1,6 +1,15 @@
+from flask import render_template
 from server import app
+
+API_PREFIX = '/api/v1'
+
 
 @app.route('/')
 @app.route('/index')
 def index():
-    return 'Hello World!'
+    return render_template('index.html', title="test Title")
+
+
+@app.route(API_PREFIX + '/')
+def api_root():
+    return 'API ROOT'
