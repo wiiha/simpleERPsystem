eb9a45e9707212ea7e1faab917c1dbf1eb48d94c
Wilhelm Hansson <wille.live@gmail.com>
2020-03-07 13:51:34 +0100

    added transaction table

--- server/models.py
+++ server/models.py
@@ -1,3 +1,4 @@
+from datetime import datetime
 from server import db
 
 
@@ -8,11 +9,23 @@ class Product(db.Model):
     price = db.Column(db.Integer)
 
     def __repr__(self):
-        return '<Product {} | {} | {}>'.format(self.id,self.product_nr,self.name)
+        return '<Product {} | {} | {}>'.format(self.id, self.product_nr, self.name)
+
 
 class StockLocation(db.Model):
     stock_nr = db.Column(db.Integer, primary_key=True)
     city = db.Column(db.String(100), index=True, unique=True)
 
     def __repr__(self):
-        return '<StockLocation {} | {}>'.format(self.stock_nr,self.city)
+        return '<StockLocation {} | {}>'.format(self.stock_nr, self.city)
+
+
+class Transaction(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    quantity = db.Column(db.Integer)
+    timestamp = db.Column(db.DateTime, index=True, default=datetime.utcnow)
+    stock_nr = db.Column(db.Integer, db.ForeignKey('stock_location.stock_nr'))
+    product_id = db.Column(db.Integer, db.ForeignKey('product.id'))
+
+    def __repr__(self):
+        return '<Transaction {} | {} | {} | {}>'.format(self.id, self.stock_nr, self.product_id, self.timestamp)
