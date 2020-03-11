c925d4885f793af5d8d44a0b99ca818ebb2fe424
Wilhelm Hansson <wille.live@gmail.com>
2020-03-11 12:50:10 +0100

    Basic routing set up

--- simple-erp-web-client/src/app/app-routing.module.ts
+++ simple-erp-web-client/src/app/app-routing.module.ts
@@ -1,11 +1,28 @@
-import { NgModule } from '@angular/core';
-import { Routes, RouterModule } from '@angular/router';
+import { NgModule } from "@angular/core";
+import { Routes, RouterModule } from "@angular/router";
 
+import { StartMenuComponent } from "./start-menu/start-menu.component";
 
-const routes: Routes = [];
+const routes: Routes = [
+  // {
+  //   path: "/",
+  //   component: LoginComponent,
+  //   data: { authGuardPipe: redirectLoggedInToHome }
+  // },
+  {
+    path: "",
+    component: StartMenuComponent,
+    pathMatch: "full" //,
+    // canActivate: [UserLoggedInGuard,UserVerifiedGuard]
+  },
+  {
+    path: "**",
+    redirectTo: "/"
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
--- simple-erp-web-client/src/app/app.module.ts
+++ simple-erp-web-client/src/app/app.module.ts
@@ -4,11 +4,13 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NavbarComponent } from './navbar/navbar.component';
+import { StartMenuComponent } from './start-menu/start-menu.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    NavbarComponent
+    NavbarComponent,
+    StartMenuComponent
   ],
   imports: [
     BrowserModule,
