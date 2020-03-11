38bd9f706d24ae55f1d37e37b38fedfee4c5b5e9
Wilhelm Hansson <wille.live@gmail.com>
2020-03-11 14:56:09 +0100

    Added product selection

--- simple-erp-web-client/src/app/transaction-module/transaction-module.component.ts
+++ simple-erp-web-client/src/app/transaction-module/transaction-module.component.ts
@@ -5,14 +5,19 @@ interface stockLocation {
   city: string;
 }
 
+interface product {
+  id: number;
+  productNr: string;
+  name: string;
+}
+
 @Component({
   selector: "app-transaction-module",
   templateUrl: "./transaction-module.component.html",
   styleUrls: ["./transaction-module.component.sass"]
 })
 export class TransactionModuleComponent implements OnInit {
-
-  stockLocations : stockLocation[]=[
+  stockLocations: stockLocation[] = [
     {
       stockNumber: 1,
       city: "Norrköping"
@@ -25,7 +30,25 @@ export class TransactionModuleComponent implements OnInit {
       stockNumber: 3,
       city: "Lund"
     }
-  ]
+  ];
+
+  products: product[] = [
+    {
+      id: 1,
+      productNr: "P001",
+      name: "jTelefon"
+    },
+    {
+      id: 2,
+      productNr: "P002",
+      name: "jPlatta"
+    },
+    {
+      id: 3,
+      productNr: "P003",
+      name: "Päronklocka"
+    },
+  ];
 
   constructor() {}
 
